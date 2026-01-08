d4b5c2923c51790144256cc9e85adafa69991945
whisky1122 <whiskycheats@gmail.com>
2026-01-08 23:17:21 +0530

    Update multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,15 @@
 import multer from 'multer'
 
+import os from 'os'
+
 let storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./public")
+    destination: (req, file, cb) => {
+        cb(null, os.tmpdir())
     },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
 });
- let upload = multer({storage})
+let upload = multer({ storage })
 
- export default upload
+export default upload
