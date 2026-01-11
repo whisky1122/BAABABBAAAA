b8eec70e244b7fd5f262a62fd736ba0407b65901
whisky1122 <whiskycheats@gmail.com>
2026-01-11 18:13:05 +0530

    8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,50 +17,53 @@ import { ToastContainer } from 'react-toastify';
 import NotFound from './pages/NotFound'
 
 function App() {
-let {userData} = useContext(userDataContext)
-let location = useLocation()
-  
+  let { userData } = useContext(userDataContext)
+  let location = useLocation()
+
   return (
     <>
-    <ToastContainer />
-    {userData && <Nav/>}
+      <ToastContainer />
+      {userData && <Nav />}
       <Routes>
 
-        <Route path='/login' 
-        element={userData ? (<Navigate to={location.state?.from || "/"}/> ) 
-        : (<Login/>)
-          }/>
+        <Route path='/login'
+          element={userData ? (<Navigate to={location.state?.from || "/"} />)
+            : (<Login />)
+          } />
 
-        <Route path='/signup' 
-        element={userData ? (<Navigate to={location.state?.from || "/"}/> ) 
-        : (<Registration/>)}/>
+        <Route path='/signup'
+          element={userData ? (<Navigate to={location.state?.from || "/"} />)
+            : (<Registration />)} />
 
-        <Route path='/' 
-        element={userData ? <Home/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
-      
-        <Route path='/about' 
-        element={userData ? <About/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/'
+          element={userData ? <Home /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-        <Route path='/collection' 
-        element={userData ? <Collections/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/about'
+          element={userData ? <About /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-        <Route path='/product' 
-        element={userData ? <Product/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/collection'
+          element={userData ? <Collections /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-        <Route path='/contact' 
-        element={userData ? <Contact/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
-        <Route path='/productdetail/:productId' 
-        element={userData ? <ProductDetail/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/product'
+          element={userData ? <Product /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-        <Route path='/cart' 
-        element={userData ? <Cart/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/contact'
+          element={userData ? <Contact /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
+        <Route path='/productdetail/:productId'
+          element={userData ? <ProductDetail /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-          <Route path='/placeorder' 
-        element={userData ? <PlaceOrder/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
-         <Route path='/order' 
-        element={userData ? <Order/> : <Navigate to="/login" state={{from: location.pathname}} /> }/>
+        <Route path='/cart'
+          element={userData ? <Cart /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
 
-        <Route path='*' element={<NotFound/>}/>
+        <Route path='/placeorder'
+          element={userData ? <PlaceOrder /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
+        <Route path='/order'
+          element={userData ? <Order /> : <Navigate to="/login" state={{ from: location.pathname }} />} />
+
+        {/* Failsafe for Render Redirects */}
+        <Route path='/index.html' element={<Navigate to="/" replace />} />
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
 
     </>
